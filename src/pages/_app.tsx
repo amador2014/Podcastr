f43c5746a18026144664304e03f2e4fe70713c66
Gabriel Amador <gabrielramador2014@gmail.com>
2021-04-21 23:25:25 -0300

    feat: add header and player components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
+import styles from "../styles/app.module.scss";
+
+import { Header } from "../components/Header/index";
+import { Player } from "../components/Player/index";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
