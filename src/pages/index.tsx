45f4772e25fc86ab5c0fcdcec1355a3e56a9719d
Gabriel Amador <gabrielramador2014@gmail.com>
2021-04-22 23:56:31 -0300

    feat: add player component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 
-export default function Home() {
+export default function Home({ episodes }) {
+  useEffect(() => {}, []);
+
   return (
-    <div >
+    <div>
       <h1>Home</h1>
+      <p>{JSON.stringify(episodes)}</p>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidade: 60 * 60 * 8
+  };
+};
